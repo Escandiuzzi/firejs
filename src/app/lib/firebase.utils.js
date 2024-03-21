dc052fc67e1a73be5e87b2f651ce57c9f2eefc7c
Luiz Felipe Escandiuzzi <37418595+Escandiuzzi@users.noreply.github.com>
2024-03-21 17:55:10 -0300

    Create firebase.utils.js

---
+++ src/app/lib/firebase.utils.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDx1glLZEJsgmQUiv5fjmVoPIXTzbMKTlE",
+    authDomain: "firescript-78734.firebaseapp.com",
+    projectId: "firescript-78734",
+    storageBucket: "firescript-78734.appspot.com",
+    messagingSenderId: "450035952547",
+    appId: "1:450035952547:web:cddf7820a32375a779c816",
+    measurementId: "G-LM6WXF03G6"
+};
+
+export const app = initializeApp(firebaseConfig);
